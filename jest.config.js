7063354339a3cf9784094e017597c84452374769
mateus-f-torres <mateusfigtorres@gmail.com>
2018-09-12 17:53:23 -0300

    add flow-typed and polished

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>__tests__/setup/setupEnzyme.js',
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  testPathIgnorePatterns: ['<rootDir>/.c9/', 'setupEnzyme.js'],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/.c9/',
+    'setupEnzyme.js'
+  ],
   moduleNameMapper: {
     // e.g.
     // "^Styles/(.*)$": "<rootDir>/src/assets/stylesheets/$1",
