6fcc65c10f62ae3b4e3a2a90c550f3acc2b6b84e
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-04 12:56:55 -0300

    build(postcss): cleanup postcss config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,3 @@
-// TODO: read about postcss-preset-env
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-custom-properties'),
-    // TODO: read about require('postcss-nesting'),
-  ]
-};
+  plugins: [require('autoprefixer'), require('postcss-custom-properties')],
+}
