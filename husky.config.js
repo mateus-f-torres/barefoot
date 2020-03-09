e39affbedb65ee953e8d525466db7eca38ddf499
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-09 11:27:55 -0300

    build(husky): add commitlint to husky

--- husky.config.js
+++ husky.config.js
@@ -2,5 +2,6 @@ module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 }
