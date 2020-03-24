ee1f4027f0dd4972c3adc4ac539ceef742102b05
mateus-f-torres <mateusfigtorres@gmail.com>
2020-03-24 17:22:04 -0300

    test(cypress): write cypress assertions

--- cypress/integration/sample_sync_spec.js
+++ cypress/integration/sample_sync_spec.js
@@ -2,6 +2,6 @@ describe('Barefoot', function() {
   it('successfully loads', function() {
     cy.visit('/')
 
-    cy.findByText('Do you want to be my friend ?')
+    cy.findByText('Do you want to be my friend ?').should('exist')
   })
 })
