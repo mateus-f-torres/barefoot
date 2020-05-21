302c92da16e2b43c89a918897927da7cbb3590ce
mateus-f-torres <mateusfigtorres@gmail.com>
2020-05-21 11:50:52 -0300

    refactor(redux): remove redux
    Simplify project
    Write about it in the documentation

--- src/stores/reducers.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import question from './question/question'
-
-const reducers = {
-  question: question,
-}
-
-export default combineReducers(reducers)
--- src/stores/sagas.js
+++
@@ -1,6 +0,0 @@
-import {all} from 'redux-saga/effects'
-import {watchRequestRandomAnswer} from './question/question'
-
-export default function* rootSaga() {
-  yield all([watchRequestRandomAnswer()])
-}
