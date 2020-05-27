98e2d8e95fa66274280f74e0c18b2d8d06fe031c
mateus-f-torres <mateusfigtorres@gmail.com>
2020-05-27 18:14:32 -0300

    fix(sw): redirect navigation to SPA shell when offline
    Instead of trying to serve some /color-game.html server /index.html;
    This way even when offline the service-worker works correctly;

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,7 +1,10 @@
 import {skipWaiting, clientsClaim} from 'workbox-core'
-import {precacheAndRoute} from 'workbox-precaching'
+import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching'
+import {registerRoute, NavigationRoute} from 'workbox-routing'
 
 skipWaiting()
 clientsClaim()
 
 precacheAndRoute(self.__WB_MANIFEST)
+
+registerRoute(new NavigationRoute(createHandlerBoundToURL('/index.html')))
