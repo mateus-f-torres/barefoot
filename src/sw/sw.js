629d5542f37a41961ac302f100637151c51a8c99
mateus-f-torres <mateusfigtorres@gmail.com>
2020-05-29 10:49:18 -0300

    fix(sw): wIP: start working on error
    Steps to test:
      1. Open clean tab (no sw cache or memory cache)
      2. Let sw install itself and populate cache
      3. In Network tab Disable Cache (memory cache) and go Offline
      4. Check if sw cache is served correctly

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -5,6 +5,26 @@ import {registerRoute, NavigationRoute} from 'workbox-routing'
 skipWaiting()
 clientsClaim()
 
+// TODO:
+/*
+ * urlManipulation falha por content-enconding e content-type errados
+ * deveria usar registerRoute e criar propria Response ?
+ * ou usar cacheable-response para imitar a Response do Netlify ?
+ *
+ * hummmmmmm...
+ *
+ * verificar onde é que Brotli é descomprimido
+ * será no servidor antes de enviar ou pelo cliente ao receber ??
+ *
+  precacheAndRoute(self.__WB_MANIFEST, {
+    urlManipulation: ({url}) => {
+      if (url.pathname.endsWith('.js') || url.pathname.endsWith('.css')) {
+        const urlWithBrotli = new URL(url.href + '.br')
+        console.log(url, urlWithBrotli)
+        return [urlWithBrotli, url]
+      }
+    },
+  })
+*/
 precacheAndRoute(self.__WB_MANIFEST)
-
 registerRoute(new NavigationRoute(createHandlerBoundToURL('/index.html')))
