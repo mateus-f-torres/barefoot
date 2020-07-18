a4009bdf2a4d9d9cd3e43af9b5d3c8cc7889ec94
mateus-f-torres <mateusfigtorres@gmail.com>
2020-07-18 19:46:04 -0300

    feat(home.js): create custom controlled Loader for dynamic imports

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,65 +1,82 @@
-import React, {Suspense} from 'react'
+import React from 'react'
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 import './Home.css'
 
-const ColorGame = React.lazy(() =>
-  import(
+async function getColorGame() {
+  const {default: ColorGame} = await import(
     /* webpackChunkName: "color-game" */
     /* webpackPrefetch: 2 */
     '../ColorGame/ColorGame'
-  ),
-)
+  )
 
-const TypographyGame = React.lazy(() =>
-  import(
+  return ColorGame
+}
+
+async function getTypographyGame() {
+  const {default: TypographyGame} = await import(
     /* webpackChunkName: "typography-game" */
     /* webpackPrefetch: 1 */
     '../TypographyGame/TypographyGame'
-  ),
-)
+  )
 
-/* How to stop Flash of Loading Content with Code-Splitting ?
- *  - Should we set a delay before showing loader ?
- *  - Or should we always wait a set amount of time ?
- *
- *  see how dynamic imports work
- *  maybe its better to now use .lazy and Suspense ?
- *  use .hydrate and really cut off a section of the app ?
- */
+  return TypographyGame
+}
+
+function Loader(props) {
+  const ComponentRef = React.useRef()
+  const [timeout, setTimeout] = React.useState(false)
+
+  React.useEffect(() => {
+    window.setTimeout(removeTimer, 3000)
+    props.lazy().then(setComponentRef, console.error)
+  }, [])
+
+  function removeTimer() {
+    setTimeout(true)
+  }
+
+  function setComponentRef(component) {
+    ComponentRef.current = component
+  }
+
+  return timeout && ComponentRef.current ? (
+    <ComponentRef.current />
+  ) : (
+    <div>loading...</div>
+  )
+}
 
 function Home() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route path="/" exact>
-            <div className="home">
-              <h1>Lets play some games!</h1>
-              <ul>
-                <li>
-                  <Link to="color-game">Color Game</Link>
-                </li>
-                <li>
-                  <Link to="typography-game">Typography Game</Link>
-                </li>
-                <li>
-                  <Link to="about-page">About page</Link>
-                </li>
-              </ul>
-            </div>
-          </Route>
-          <Route path="/color-game">
-            <ColorGame />
-          </Route>
-          <Route path="/typography-game">
-            <TypographyGame />
-          </Route>
-          <Route path="/about-page">
-            <h1>About page</h1>
-            <p>work in progress</p>
-          </Route>
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route path="/" exact>
+          <div className="home">
+            <h1>Lets play some games!</h1>
+            <ul>
+              <li>
+                <Link to="color-game">Color Game</Link>
+              </li>
+              <li>
+                <Link to="typography-game">Typography Game</Link>
+              </li>
+              <li>
+                <Link to="about-page">About page</Link>
+              </li>
+            </ul>
+          </div>
+        </Route>
+        <Route path="/color-game">
+          <Loader lazy={getColorGame} />
+        </Route>
+        <Route path="/typography-game">
+          <Loader lazy={getTypographyGame} />
+        </Route>
+        <Route path="/about-page">
+          <h1>About page</h1>
+          <p>work in progress</p>
+        </Route>
+      </Switch>
     </BrowserRouter>
   )
 }
