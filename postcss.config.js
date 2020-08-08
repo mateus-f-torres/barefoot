15c6afc3e9026e4c010a5963a87f06b06190a534
mateus-f-torres <mateusfigtorres@gmail.com>
2020-08-08 11:37:00 -0300

    build(tailwind): start tailwindcss integration

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('postcss-custom-properties')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-custom-properties'),
+  ],
 }
