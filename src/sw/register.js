bdeb5285ad017664a6d1955d3856b56f122036e3
mateus-f-torres <mateusfigtorres@gmail.com>
2020-11-21 09:59:45 -0300

    chore(sw/register.js): add TODO

--- src/sw/register.js
+++ src/sw/register.js
@@ -1,3 +1,4 @@
+// TODO: use webpack env plugin to bail in NODE_ENV=development
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(console.log, console.error)
