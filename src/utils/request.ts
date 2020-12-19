0df3e5eb1f0fa802a5fb95d54f3027933000e2b6
mateus-f-torres <mateusfigtorres@gmail.com>
2020-12-19 13:48:24 -0300

    refactor(request.ts): convert promise to async/await

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,20 +1,12 @@
-export default function request(url: string, options = {method: 'GET'}) {
-  return fetch(url, options)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('HTTP request error, status = '.concat(String(res.status)))
-      } else if (res.status == 204) {
-        return null
-      } else {
-        return res
-          .json()
-          .then((body) => body)
-          .catch((e) => {
-            throw e
-          })
-      }
-    })
-    .catch((e) => {
-      throw e
-    })
+async function request(url: string, options = {method: 'GET'}) {
+  const res = await fetch(url, options)
+  if (!res.ok) {
+    throw new Error('HTTP request error, '.concat(String(res.status)))
+  } else if (res.status == 204) {
+    return null
+  } else {
+    return res.json()
+  }
 }
+
+export default request
