d364d4b313f25f494a8a460e5be0441097e04a28
mateus-f-torres <mateusfigtorres@gmail.com>
2020-12-27 12:43:00 -0300

    chore: annotate basic return type
    Needed for a recommended lint rule;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="max-w-lg mx-auto mt-24">
       <h1 className="font-body text-4xl font-bold text-regal-blue text-center">
