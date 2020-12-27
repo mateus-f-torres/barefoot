4d4dc94f5b04f865b8204cc294ad7d586061390b
mateus-f-torres <mateusfigtorres@gmail.com>
2020-12-27 13:38:24 -0300

    chore: pass ESLint and Typescript compilation
    Use "checkJS" but disable for sw files;
    Add libs "es6", "dom" and "webworker" for project;
    Remove commented ESLint config lines;

--- src/sw/register.js
+++ src/sw/register.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 if ("serviceWorker" in navigator && PRODUCTION) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js").then(console.log, console.error)
--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import {clientsClaim} from "workbox-core"
 import {precacheAndRoute, createHandlerBoundToURL} from "workbox-precaching"
 import {registerRoute, NavigationRoute} from "workbox-routing"
