4351bf7fe90032597ad685e0ab697fe7cb2896d0
mateus-f-torres <mateusfigtorres@gmail.com>
2021-03-23 15:02:59 -0300

    refactor(server.sh): replace hard-coded image name with variable

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+docker_name="barefoot"
+
 # test if the following command returns a container id
-docker_id="$(docker ps --quiet --filter name=barefoot)"
+docker_id="$(docker ps --quiet --filter name=$docker_name)"
 
 # run only if already present
 if test -n "$docker_id"; then
-  docker stop barefoot
-  docker rm barefoot
+  docker stop $docker_name
+  docker rm $docker_name
 fi
 
 # build a clean new image
-docker build --tag barefoot .
+docker build --tag $docker_name .
 
 # run image, serving our project on a ngx server mapped to port 8080
-docker run --name barefoot --detach --publish 8080:80 barefoot
+docker run --name $docker_name --detach --publish 8080:80 $docker_name
