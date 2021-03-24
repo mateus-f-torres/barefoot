3ad5f8c4829495bd5afe98cbfa88aa9fb3b82259
mateus-f-torres <mateusfigtorres@gmail.com>
2021-03-24 10:04:54 -0300

    feat(server.sh): allow developer to assign local port for docker

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-docker_name="barefoot"
+image_name="barefoot"
+local_port="$(test -n "$1" && echo "$1" || echo "8080")"
 
-# test if the following command returns a container id
-docker_id="$(docker ps --quiet --filter name=$docker_name)"
+# get barefoot container id, if present
+docker_id="$(docker ps --quiet --filter name=$image_name)"
 
-# run only if already present
+# remove old barefoot image, if present
 if test -n "$docker_id"; then
-  docker stop $docker_name
-  docker rm $docker_name
+  docker stop $image_name && docker rm $image_name
 fi
 
-# build a clean new image
-docker build --tag $docker_name .
+# build a new image
+docker build --tag $image_name .
 
-# run image, serving our project on a ngx server mapped to port 8080
-docker run --name $docker_name --detach --publish 8080:80 $docker_name
+# run image, mapping our local port to nginx port 80
+docker run --name $image_name --detach --publish "$local_port":80 $image_name
