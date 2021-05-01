566beca14d0ed5b0618294f3a10d96ab2170fb16
mateus-f-torres <mateusfigtorres@gmail.com>
2021-05-01 08:57:09 -0300

    feat(index.tsx): add <React.StrictMode/> around <App/>

--- src/index.tsx
+++ src/index.tsx
@@ -9,5 +9,10 @@ import App from "./components/App/App"
 const root = document.getElementById("root")
 
 if (root !== null) {
-  ReactDOM.render(<App />, root)
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  )
 }
