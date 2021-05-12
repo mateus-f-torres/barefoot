5cfeb13e85530211e3bd2b38c7e4b568dd124c81
mateus-f-torres <mateusfigtorres@gmail.com>
2021-05-12 20:18:37 -0300

    refactor: use button to ask permission for notification

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,36 @@
 import React from "react"
+import * as PusherPushNotifications from "@pusher/push-notifications-web"
+
+declare global {
+  const PUSHER_INSTANCE_ID: string
+}
 
 function App(): React.ReactElement {
+  async function handleSubscribe(): Promise<void> {
+    const swRegistration = await window.navigator.serviceWorker.getRegistration()
+    if (swRegistration !== undefined) {
+      const beamsClient = new PusherPushNotifications.Client({
+        instanceId: "542b5fae-8e7a-405b-8750-43086af04cb1",
+        // instanceId: PUSHER_INSTANCE_ID,
+        serviceWorkerRegistration: swRegistration,
+      })
+
+      beamsClient
+        .start()
+        .then(async () => await beamsClient.addDeviceInterest("debug-barefoot"))
+        .then(() => console.log("Successfully registered and subscribed!"))
+        .catch(console.error)
+    } else {
+      console.log("No service worker registered.")
+    }
+  }
+
   return (
     <div className="max-w-lg mx-auto mt-24">
       <h1 className="font-body text-4xl font-bold text-regal-blue text-center">
         Hello World
       </h1>
+      <button onClick={handleSubscribe}>Subscribe!</button>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import * as PusherPushNotifications from "@pusher/push-notifications-web"
 
 import "./index.css"
 import "./sw/register"
 
 import App from "./components/App/App"
-
-declare global {
-  const PUSHER_INSTANCE_ID: string
-}
 
 const root = document.getElementById("root")
 
@@ -21,18 +16,3 @@ if (root !== null) {
     root
   )
 }
-
-window.navigator.serviceWorker.ready
-  .then((registration) => {
-    const beamsClient = new PusherPushNotifications.Client({
-      instanceId: PUSHER_INSTANCE_ID,
-      serviceWorkerRegistration: registration,
-    })
-
-    beamsClient
-      .start()
-      .then(async () => await beamsClient.addDeviceInterest("debug-barefoot"))
-      .then(() => console.log("Successfully registered and subscribed!"))
-      .catch(console.error)
-  })
-  .catch(() => {})
