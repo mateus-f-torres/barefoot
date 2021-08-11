062b5647cf63e78e1441cc17b3b5f861d6ec2f1a
Mateus F Torres <mateusfigtorres@gmail.com>
2021-08-11 11:06:49 -0300

    fix: correct code and commit message errors

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import "./sw/register"
 
-var Fail_CI = null;
-
 import App from "./components/App/App"
 
 const root = document.getElementById("root")
