088eb0b94e218c476ae9a34d796c12349870f3d0
mateus-f-torres <mateusfigtorres@gmail.com>
2021-11-03 09:41:15 -0300

    test(app): initial App.spec.tsx development

---
+++ src/components/App/App.spec.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {render, waitFor, screen} from "@testing-library/react"
+
+import App from "./App"
+
+describe("App", function () {
+  it("should display 'Hello World'", async function () {
+    render(<App />)
+
+    await waitFor(() => {
+      expect(screen.queryByText("Hello World")).toBeInTheDocument()
+    })
+  })
+})
