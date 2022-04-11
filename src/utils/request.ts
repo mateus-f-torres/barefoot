7adfb99c149c55717458cd0d4f9a2ebe4f693d0e
Mateus Torres <mateusfigtorres@gmail.com>
2022-04-11 20:14:19 -0300

    refactor(eslint): remove custom rules and fix new errors

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,7 +4,7 @@ async function request(url: string, options: any): Promise<unknown> {
   const res = await fetch(url, options)
   if (!res.ok) {
     throw new Error("HTTP request error, ".concat(String(res.status)))
-  } else if (res.status == 204) {
+  } else if (res.status === 204) {
     return null
   } else {
     return await res.json()
