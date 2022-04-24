3ae08589edd9952485f35536d0dcebd8e1eb62f5
Mateus Torres <mateusfigtorres@gmail.com>
2022-04-24 10:47:46 -0300

    build(babel): update corejs version to v3.22

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", {useBuiltIns: "usage", corejs: "3.21"}],
+    ["@babel/preset-env", {useBuiltIns: "usage", corejs: "3.22"}],
     ["@babel/preset-react"],
     ["@babel/preset-typescript"],
   ],
