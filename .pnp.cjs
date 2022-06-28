fdb25c7f8c0a032dc922290ba63ef44f7139ae01
Mateus Torres <mateusfigtorres@gmail.com>
2022-06-28 18:54:41 -0300

    chore(deps): update dependency core-js to v3.23.3

--- .pnp.cjs
+++ .pnp.cjs
@@ -55,7 +55,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["commitizen", "npm:4.2.4"],\
             ["compression-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:10.0.0"],\
             ["copy-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:11.0.0"],\
-            ["core-js", "npm:3.22.8"],\
+            ["core-js", "npm:3.23.3"],\
             ["css-loader", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:6.7.1"],\
             ["css-minimizer-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:4.0.0"],\
             ["cz-conventional-changelog", "npm:3.3.0"],\
@@ -8702,7 +8702,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["commitizen", "npm:4.2.4"],\
             ["compression-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:10.0.0"],\
             ["copy-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:11.0.0"],\
-            ["core-js", "npm:3.22.8"],\
+            ["core-js", "npm:3.23.3"],\
             ["css-loader", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:6.7.1"],\
             ["css-minimizer-webpack-plugin", "virtual:d0d1c83cb0f00b570c3a159645ed5194738e2467e351a0af2263233a889d308278918968f4046da19e8b1a8723f430f430702603f0598874396a7c7e1956daf0#npm:4.0.0"],\
             ["cz-conventional-changelog", "npm:3.3.0"],\
@@ -10114,10 +10114,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["core-js", [\
-        ["npm:3.22.8", {\
-          "packageLocation": "./.yarn/unplugged/core-js-npm-3.22.8-40b5d0770b/node_modules/core-js/",\
+        ["npm:3.23.3", {\
+          "packageLocation": "./.yarn/unplugged/core-js-npm-3.23.3-83cb265bcd/node_modules/core-js/",\
           "packageDependencies": [\
-            ["core-js", "npm:3.22.8"]\
+            ["core-js", "npm:3.23.3"]\
           ],\
           "linkType": "HARD"\
         }]\
